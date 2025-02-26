bc504518e0c3e569c195cb71867a0af2ebc937fd
DailyDev59 <daily1963@yandex.ru>
2025-02-26 11:57:12 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="background">
-    <Calculator/>
+    <Calculator />
   </div>
 </template>
 
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,25 +5,27 @@
     </button>
     <div class="display">
       <p class="calc">
-        {{ output || '0' }}
+        {{ expression }}{{ currentInput }}
       </p>
-      <p class="result">{{ subtotal || '0' }}</p>
+      <p class="result">
+        <div v-if="subtotal !== null"> {{ subtotal }}</div>
+      </p>
     </div>
     <div class="keyboard-up">
-      <button @click="clearLastCharacter" class="brown">Backspace</button>
-      <button @click="clearLastNumber" class="brown">CE</button>
-      <button @click="clearCalculator" class="brown">C</button>
+      <button class="brown">Backspace</button>
+      <button class="brown">CE</button>
+      <button @click="clear" class="brown">C</button>
     </div>
     <div class="keyboard">
-      <button @click="clearItem" class="gray">
+      <button class="gray">
         <img src="./../assets/img/mc.svg" alt="mc" />
       </button>
-      <button @click="saveItem" class="gray ms">ms</button>
-      <button @click="minusItem" class="gray ms">m&ndash;</button>
-      <button @click="plusItem" class="gray">
+      <button class="gray ms">ms</button>
+      <button class="gray ms">m&ndash;</button>
+      <button class="gray">
         <img src="./../assets/img/m+.svg" alt="m+" />
       </button>
-      <button @click="negateValue" class="brown">
+      <button class="brown">
         <img src="./../assets/img/plus-minus.svg" alt="plus-minus" />
       </button>
       <button @click="enterNum('%')" class="brown">
@@ -33,10 +35,10 @@
           alt="percentage"
         />
       </button>
-      <button @click="enterNum('÷')" class="brown">
+      <button @click="handleOperator('÷')" class="brown">
         <img src="./../assets/img/divide.svg" alt="divide" />
       </button>
-      <button @click="enterNum('x')" class="brown">
+      <button @click="handleOperator('x')" class="brown">
         <img src="./../assets/img/multiply.svg" alt="multiply" />
       </button>
       <button @click="enterNum('7')" class="black">
@@ -48,7 +50,7 @@
       <button @click="enterNum('9')" class="black">
         <img src="./../assets/img/nine.svg" alt="nine" />
       </button>
-      <button @click="enterNum('-')" class="brown">
+      <button @click="handleOperator('-')" class="brown">
         <img src="./../assets/img/minus.svg" alt="minus" />
       </button>
       <button @click="enterNum('4')" class="black">
@@ -60,7 +62,7 @@
       <button @click="enterNum('6')" class="black">
         <img src="./../assets/img/six.svg" alt="six" />
       </button>
-      <button @click="enterNum('+')" class="brown">
+      <button @click="handleOperator('+')" class="brown">
         <img src="./../assets/img/plus.svg" alt="plus" />
       </button>
       <button @click="enterNum('1')" class="black">
@@ -72,7 +74,7 @@
       <button @click="enterNum('3')" class="black">
         <img src="./../assets/img/three.svg" alt="three" />
       </button>
-      <button @click="calculate(numOne, numTwo, operator)" class="orange">
+      <button @click="handleEqual" class="orange">
         <img src="./../assets/img/equal.svg" alt="equal" />
       </button>
       <button @click="enterNum('0')" class="black box0">
@@ -88,158 +90,76 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-type op = '+' | '-' | 'x' | '÷' | '%'
+const expression = ref('')
+const currentInput = ref('')
+const subtotal = ref<number | null>(null)
 
-let output = ref('')
-const numOne = ref('')
-const numTwo = ref('')
-let subtotal = ref('')
-const operator = ref('')
+const enterNum = (digit: string) => {
+  currentInput.value += digit
+  calculateIntermediate()
+}
 
-const ops = ['+', '-', 'x', '÷', '%']
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ',']
-const arrayNumTwo = ref<string[]>([])
+const handleOperator = (operator: string) => {
+  if (currentInput.value) {
+    expression.value += currentInput.value + operator
+    currentInput.value = ''
+    calculateIntermediate()
+  } else if (expression.value) {
+    // Replace the last operator
+    expression.value = expression.value.slice(0, -1) + operator
+    calculateIntermediate()
+  }
+}
 
-let items: string[] = []
-const item = ref(items[0])
+const calculateIntermediate = () => {
+  const expr = expression.value + currentInput.value
+  subtotal.value = evaluateExpression(expr)
+}
 
-const enterNum = (val: string) => {
-  // if 0-9 entered
-  if (numbers.includes(val)) {
-    if (!numTwo.value && !operator.value) {
-      numOne.value += val
-      output.value = numOne.value
+const  evaluateExpression = (expr: string): number | null => {
+  if (!expr) return null
+
+  // Tokenize the expression
+  const tokens = expr.match(/(\d+|\+|-|x|÷)/g)
+  if (!tokens) return null
+
+  const stack: number[] = []
+  let currentOperator: string | null = null
+
+  for (const token of tokens) {
+    if (/\d+/.test(token)) {
+      const number = parseFloat(token)
+      if (currentOperator) {
+        const prevNumber = stack.pop()!
+        if (currentOperator === '+') stack.push(prevNumber + number)
+        else if (currentOperator === '-') stack.push(prevNumber - number)
+        else if (currentOperator === 'x') stack.push(prevNumber * number)
+        else if (currentOperator === '÷') stack.push(prevNumber / number)
+        currentOperator = null
+      } else {
+        stack.push(number)
+      }
+    } else if (/[\+-x÷]/.test(token)) {
+      currentOperator = token
     }
-    if (numOne.value && operator.value) {
-      numTwo.value += val
-      output.value = numTwo.value
-    }
-    console.log(numOne.value, operator.value, numTwo.value)
   }
 
-  // if + - x ÷ entered
-  if (ops.includes(val)) {
-    operator.value = val
-    output.value = operator.value
-    // subtotal.value = ''
-    console.log(numOne.value, operator.value, numTwo.value)
+  return stack.length > 0 ? stack[0] : null
+}
+
+const handleEqual = () => {
+  if (currentInput.value) {
+    expression.value += currentInput.value
+    currentInput.value = ''
+    calculateIntermediate()
+    expression.value = '' // Reset after calculation
   }
 }
 
-const calculate = (a: string, b: string, op: string): string => {
-  switch (op) {
-    case '+':
-      a = (+a + +b).toString()
-      break
-    case '-':
-      a = (+a - +b).toString()
-      break
-    case 'x':
-      a = (+a * +b).toString()
-      break
-    case '÷':
-      a = (+a / +b).toString()
-      break
-    case '%':
-      return (a = (+a / 100).toString())
-  }  
-  numOne.value = a
-  subtotal.value = a
-  numTwo.value = ''  
-  output.value = numOne.value
-  console.log(numOne.value, operator.value, numTwo.value)
-}
-
-const clearCalculator = () => {
-  numOne.value = ''
-  operator.value = ''
-  numTwo.value = ''
-  output.value = ''
-  subtotal.value = ''
-  clearItem()
-}
-
-const clearLastNumber = () => {
-  if (operator.value) {
-    numTwo.value = ''
-    subtotal.value = ''
-    operator.value = ''
-    numOne.value = ''
-  }
-}
-
-const clearLastCharacter = () => {
-  // Combine all components into a single string
-  const currentExpression = numOne.value + operator.value + numTwo.value
-
-  if (currentExpression.length === 0) return
-
-  // Remove last character
-  const newExpression = currentExpression.slice(0, -1)
-
-  // Parse the new expression back into components
-  const opIndex = newExpression.search(/[+\-x÷]/)
-
-  if (opIndex === -1) {
-    // No op found - treat everything as numOne
-    numOne.value = newExpression
-    operator.value = ''
-    numTwo.value = ''
-  } else {
-    // Split into numOne, op, and numTwo
-    numOne.value = newExpression.slice(0, opIndex)
-    operator.value = newExpression[opIndex]
-    numTwo.value = newExpression.slice(opIndex + 1)
-  }
-}
-
-const negateValue = () => {
-  if (operator.value) {
-    // negate second operand
-    numTwo.value = toggleSign(numTwo.value)
-    output.value = numTwo.value
-  } else {
-    // negate first operand
-    numOne.value = toggleSign(numOne.value)
-    output.value = numOne.value
-  }
-}
-
-const toggleSign = (value: string): string => {
-  // start negative if empty
-  if (value === '') return '-'
-  // remove standalone
-  if (value === '-') return ''
-  return value.startsWith('-') ? value.slice(1) : `-${value}`
-}
-
-const saveItem = () => {
-  if (subtotal.value || numOne.value) {
-    items.push(subtotal.value || numOne.value)
-  }
-  if (items.length > 1) items.shift()
-
-  console.log(items[0])
-}
-
-const clearItem = () => {
-  if (items) items = []
-}
-
-const plusItem = () => {
-  if (subtotal.value || numOne.value) {
-    subtotal.value = (
-      parseFloat(items[0]) + parseFloat(numOne.value)
-    ).toString()
-  }
-}
-
-const minusItem = () => {
-  if (subtotal.value || numOne.value) {
-    subtotal.value = (
-      parseFloat(numOne.value) - parseFloat(items[0])
-    ).toString()
-  }
+const clear = () => {
+  expression.value = ''
+  currentInput.value = ''
+  subtotal.value = null
 }
 </script>
 
@@ -287,6 +207,7 @@ const minusItem = () => {
   margin: 0;
   font-size: 1.5rem;
   font-weight: bold;
+  color: #6b31e1;
   padding-right: 15px;
 }
 .keyboard-up {
